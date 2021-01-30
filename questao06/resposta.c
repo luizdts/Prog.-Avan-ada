b968f2ee819bfbced2293b1a30943145d86f850b
luizdts <77617519+luizdts@users.noreply.github.com>
2021-01-30 01:36:36 -0300

    Update and rename resposta.txt to resposta.c

--- questao06/resposta.c
+++ questao06/resposta.c
@@ -4,9 +4,9 @@ int main(void){
   printf("endereco do terceiro elemento: %d\n", &vetor+2); // captura do endereço de memória do terceiro elemento do vetor.
   printf("valor do terceiro elemento: %d", vetor+2); // captura do valor do terceiro elemento do vetor. Assim a resposta é "pulo+2".
 }
-resultado: 
-endereco do terceiro elemento: 6487592
-valor do terceiro elemento: 6487560
+// resultado: 
+// endereco do terceiro elemento: 6487592
+// valor do terceiro elemento: 6487560
 
-para observar o valor inteiro do elemento 3 é só utilizarmos: 
-printf("valor do terceiro elemento: %d", *vetor+2); // assim ele imprimirá o valor inteiro 3
+// para observar o valor inteiro do elemento 3 é só utilizarmos: 
+// printf("valor do terceiro elemento: %d", *vetor+2); // assim ele imprimirá o valor inteiro 3
