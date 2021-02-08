13ed0cb861844d952ba7ebfdd952b9b95a92f49d
luizdts <77617519+luizdts@users.noreply.github.com>
2021-02-08 15:37:23 -0300

    Update and rename codigo to resposta.c

---
+++ questao11/resposta.c
@@ -0,0 +1,14 @@
+# include<stdlib.h>
+int main(){
+  float aloha[10], coisas[10][5], *pf, value = 2.2;
+  int i=3; 
+  aloha[2] = value; // válido pois aloha recebe 2.2.
+  scanf("%f", &aloha); // válido, é interpretado o valor de aloha[0].
+  aloha = value"; // válido pois aloha é um ponteiro.
+  printf("%f", aloha); // válido, retorna a posição inicial de aloha, aloha[0].
+  coisas[4][4] = aloha[3]; // válido, a matriz recebe o conteúdo de aloha[3].
+  coisas[5] = aloha;       // inválido, a declaração está incorreta pois o termo se trata de uma matriz.
+  pf = value;       // inválido, pf é um ponteiro, têm de se usar as declarações corretas para capturar os valores ou endereços desejados.
+  pf = aloha;     // válido pois aloha é um ponteiro.
+ 
+}
