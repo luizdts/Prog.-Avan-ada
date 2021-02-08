489dbe4abe6d862973764fd35b67b523fd16e21c
luizdts <77617519+luizdts@users.noreply.github.com>
2021-02-08 18:03:08 -0300

    Update resposta.c

--- questao12/resposta.c
+++ questao12/resposta.c
@@ -1,1 +1,2 @@
-/// O ponteiro para uma função representa uma variável que é capaz de armazenar um endereço de memória ou um endereço de memória de outra variável
+/// O ponteiro para uma função representa uma variável que é capaz de armazenar um endereço de memória ou
+/// um endereço de memória de outra variável.
