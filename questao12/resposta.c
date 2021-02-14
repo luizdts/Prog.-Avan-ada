4a852e5731ab9fc23d30593a2136598f2d2ad538
luizdts <77617519+luizdts@users.noreply.github.com>
2021-02-14 18:10:02 -0300

    Update resposta.c

--- questao12/resposta.c
+++ questao12/resposta.c
@@ -1,2 +1,4 @@
 /// O ponteiro para uma função representa uma variável que é capaz de armazenar um endereço de memória ou
 /// um endereço de memória de outra variável.
+
+/// um exemplo de programa que podemos utilizar com ponteiros:
