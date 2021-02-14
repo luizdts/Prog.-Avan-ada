778a96a5a54edbef4758bbcb692c8777026341b9
luizdts <77617519+luizdts@users.noreply.github.com>
2021-02-14 20:25:22 -0300

    Update resposta.c

--- questao13/resposta.c
+++ questao13/resposta.c
@@ -1,14 +1,10 @@
 # include <stdlib.h>
-
 int main(){
   float *p1;
   int i, j, x, aux;
   x = 5;
   p1 = (float*) malloc(x*sizeof(float));
 
-  for (j=0;j<x;j++){
-      scanf("%f", p1[j]);
-  }
     for(i=0;i<x; i++){
       for(j=i;j<x;j++){
         if(p1[i]>p1[j]){
@@ -20,13 +16,12 @@ int main(){
     }
   for(j=0;j<x;j++){
     if(j == x-1){
-      printf("%f", p1[j]);
+      printf("%.2f", p1[j]);
     } 
     else {
-    printf("%f", p1[j]);
+    printf("%.2f", p1[j]);
     }
   }
   free(p1);
   return 0;
 }
-
