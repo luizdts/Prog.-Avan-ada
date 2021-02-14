b83bb082c7ca15d2c7e9038d0285e16ee626a1ca
luizdts <77617519+luizdts@users.noreply.github.com>
2021-02-14 20:05:13 -0300

    Update resposta.c

--- questao12/resposta.c
+++ questao12/resposta.c
@@ -1,4 +1,21 @@
 /// O ponteiro para uma função representa uma variável que é capaz de armazenar um endereço de memória ou
 /// um endereço de memória de outra variável.
 
-/// um exemplo de programa que podemos utilizar com ponteiros:
+/// um exemplo de programa que podemos utilizar com ponteiros
+
+
+# include<stdlib.h>
+
+int soma(int x,int y){
+   return (x + y);
+}
+
+int main(){
+  int (*p)(), n, x, y;
+  scanf("%d", &x);
+  scanf("%d", &y);
+  p = soma; // o ponteiro recebe a função soma
+  n = p(x,y); // n recebe os valores de p
+  printf("%d\n",n); // saída do valor de p
+  return 0;
+}
