02685f01b218d51abe88934a341e3e2b8a153b00
luizdts <77617519+luizdts@users.noreply.github.com>
2021-02-16 11:45:35 -0300

    Update resposta.c

--- questao16/resposta.c
+++ questao16/resposta.c
@@ -1,4 +1,5 @@
 // Pesquisando na internet, a biblioteca em C que realiza a medição do tempo de execução é chamada de time.h
 // Para realizar a medição do tempo de execução do programa devemos trabalhar com função interna chamada de clock()
-// ela é responsável por medir os tempos iniciais e finais da execução do programa, com isso também definimos variáveis específicas da biblioteca time.h
+// ela é responsável por medir os tempos iniciais e finais da execução do programa,
+// com isso também definimos variáveis específicas da biblioteca time.h
 // as variáveis devem ser declaradas com time(NULL)
